b915439698eff486f3cae8765c6f4b935ecbe985
TJohn00 <thomasbenjohnson@yahoo.co.in>
2021-02-25 23:20:07 +0530

    Added emp dashboard

--- BBMS/Login.Designer.cs
+++ BBMS/Login.Designer.cs
@@ -33,9 +33,9 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.jThinButton1 = new JThinButton.JThinButton();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.jThinButton1 = new JThinButton.JThinButton();
             this.SuspendLayout();
             // 
             // label1
@@ -43,9 +43,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Fugaz One", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(69, 29);
+            this.label1.Location = new System.Drawing.Point(52, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(576, 44);
+            this.label1.Size = new System.Drawing.Size(469, 35);
             this.label1.TabIndex = 1;
             this.label1.Text = "BLOOD BANK MANAGEMENT SYSTEM";
             // 
@@ -54,9 +55,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(166, 121);
+            this.label2.Location = new System.Drawing.Point(124, 98);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(139, 25);
+            this.label2.Size = new System.Drawing.Size(109, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Username :";
             // 
@@ -65,55 +67,40 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(166, 193);
+            this.label3.Location = new System.Drawing.Point(124, 157);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(137, 25);
+            this.label3.Size = new System.Drawing.Size(106, 18);
             this.label3.TabIndex = 3;
             this.label3.Text = "Password :";
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Century Schoolbook", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(376, 121);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(282, 98);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(204, 28);
+            this.textBox1.Size = new System.Drawing.Size(154, 23);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Century Schoolbook", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(376, 190);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(282, 154);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(204, 28);
+            this.textBox2.Size = new System.Drawing.Size(154, 23);
             this.textBox2.TabIndex = 5;
-            // 
-            // jThinButton1
-            // 
-            this.jThinButton1.BackColor = System.Drawing.Color.Transparent;
-            this.jThinButton1.BackgroundColor = System.Drawing.Color.Red;
-            this.jThinButton1.BorderColor = System.Drawing.Color.Red;
-            this.jThinButton1.BorderRadius = 20;
-            this.jThinButton1.ButtonText = "Login";
-            this.jThinButton1.Font_Size = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.jThinButton1.ForeColors = System.Drawing.Color.White;
-            this.jThinButton1.HoverBackground = System.Drawing.Color.White;
-            this.jThinButton1.HoverBorder = System.Drawing.Color.Empty;
-            this.jThinButton1.HoverFontColor = System.Drawing.SystemColors.Highlight;
-            this.jThinButton1.LineThickness = 2;
-            this.jThinButton1.Location = new System.Drawing.Point(263, 258);
-            this.jThinButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.jThinButton1.Name = "jThinButton1";
-            this.jThinButton1.Size = new System.Drawing.Size(192, 58);
-            this.jThinButton1.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(247, 343);
+            this.label4.Location = new System.Drawing.Point(185, 279);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(222, 25);
+            this.label4.Size = new System.Drawing.Size(170, 18);
             this.label4.TabIndex = 7;
             this.label4.Text = "Continue as Admin";
             // 
@@ -124,27 +111,47 @@
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(697, 9);
+            this.label5.Location = new System.Drawing.Point(523, 7);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 38);
+            this.label5.Size = new System.Drawing.Size(33, 31);
             this.label5.TabIndex = 8;
             this.label5.Text = "X";
             // 
+            // jThinButton1
+            // 
+            this.jThinButton1.BackColor = System.Drawing.Color.Transparent;
+            this.jThinButton1.BackgroundColor = System.Drawing.Color.Red;
+            this.jThinButton1.BorderColor = System.Drawing.Color.Maroon;
+            this.jThinButton1.BorderRadius = 17;
+            this.jThinButton1.ButtonText = "Login";
+            this.jThinButton1.Font_Size = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jThinButton1.ForeColors = System.Drawing.SystemColors.Control;
+            this.jThinButton1.HoverBackground = System.Drawing.Color.White;
+            this.jThinButton1.HoverBorder = System.Drawing.Color.Empty;
+            this.jThinButton1.HoverFontColor = System.Drawing.SystemColors.Highlight;
+            this.jThinButton1.LineThickness = 2;
+            this.jThinButton1.Location = new System.Drawing.Point(188, 200);
+            this.jThinButton1.Name = "jThinButton1";
+            this.jThinButton1.Size = new System.Drawing.Size(168, 63);
+            this.jThinButton1.TabIndex = 9;
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(747, 403);
+            this.ClientSize = new System.Drawing.Size(560, 327);
+            this.Controls.Add(this.jThinButton1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.jThinButton1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -159,8 +166,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private JThinButton.JThinButton jThinButton1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private JThinButton.JThinButton jThinButton1;
     }
 }
