6f7b0e841a463c88a0f2572af712149e004d9ffe
Sagar Shanbhag <sagar.shanbhag27@gmail.com>
2021-07-31 15:40:58 +0530

    Update Program.cs

--- BBMS/Program.cs
+++ BBMS/Program.cs
@@ -16,7 +16,7 @@ namespace BBMS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new emp_addPatient());
+            Application.Run(new Blink());
         }
     }
 }
